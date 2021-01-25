f0268daf6fa0c04042003ba0e699de00ec7b4d46
Edmilson <44274029+EdmilsonOSJr@users.noreply.github.com>
2021-01-25 17:04:15 -0300

    arquivos da classe professor e das calsses de acesso a BD e teste

---
+++ EdmilsonSantana-DAW/src/br/edu/barbacena/ifsudestemg/daw/dao/ProfessorDAO.java
@@ -0,0 +1,5 @@
+package br.edu.barbacena.ifsudestemg.daw.dao;
+
+public class ProfessorDAO {
+
+}
---
+++ EdmilsonSantana-DAW/src/br/edu/barbacena/ifsudestemg/daw/teste/TestarProfessorDAO.java
@@ -0,0 +1,5 @@
+package br.edu.barbacena.ifsudestemg.daw.teste;
+
+public class TestarProfessorDAO {
+
+}
